4ccfa7311c137ab5b9871ab6193bb56a5ef1a534
Chájù <61478663+jarod-2134@users.noreply.github.com>
2022-04-01 13:17:41 +0200

    Update example.java

--- Design greenfoot/example.java
+++ Design greenfoot/example.java
@@ -1,1 +1,42 @@
+abstract class Animal {
 
+    abstract void reveal();
+    abstract void swim();
+
+}
+
+class Cat extends Animal{
+
+@Override
+public void reveal(){
+System.out.println("I am a cat and I can eat you!");
+}
+public void swim(){
+System.out.println("I can swim");
+}
+}
+
+class Bear extends Animal {
+
+    @Override
+    public void reveal() {
+        System.out.println("I am a bear and I can eat you!");
+    }
+    public void swim(){
+    System.out.println("I cant swim");
+    }
+
+}
+
+
+
+class Main {
+  public static void main(String[] args) {
+    Cat myCat = new Cat();
+    Bear myBear = new Bear();
+    myBear.reveal();
+    myBear.swim();
+    myCat.reveal();
+    myCat.swim();
+  }
+}
