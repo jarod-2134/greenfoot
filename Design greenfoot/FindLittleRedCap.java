44234a8ada9bd50099e09939e9f3d317ff7455ac
Chájù <61478663+jarod-2134@users.noreply.github.com>
2022-04-11 15:56:06 +0200

    Update FindLittleRedCap.java

--- Design greenfoot/FindLittleRedCap.java
+++ Design greenfoot/FindLittleRedCap.java
@@ -2,4 +2,17 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class FindLittleRedCap implements FindInterface {
     private Actor actor;
+    
+    public void find(Actor actor){
+    this.actor=actor;
+    eatLittleRedCap(); 
+    }
+    
+     public void eatLittleRedCap()
+    {
+        if(canSee(LittleRedCap.class))
+        {
+            removeObject (LittleRedCap.class);
+        }
+    }
 }
