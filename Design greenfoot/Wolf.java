39feb9cd982001bf322a0fc7f6e5af233ae5bfd0
Chájù <61478663+jarod-2134@users.noreply.github.com>
2022-04-11 15:59:15 +0200

    Update Wolf.java

--- Design greenfoot/Wolf.java
+++ Design greenfoot/Wolf.java
@@ -3,12 +3,15 @@ import greenfoot.*;
 public class Wolf extends Actor {
 
     private MoveInterface moveInterface;
-
+    private FindInterface findInterface;
+    
     public Wolf() {
         moveInterface = new MoveRandom();
+        findInterface = new FindLittleRedCap
     }
 
     public void act() {
         moveInterface.move(this);
+        findInterface.find(this);
     }
 }
