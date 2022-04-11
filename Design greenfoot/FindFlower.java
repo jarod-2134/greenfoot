7be4eb54cfe1fcb0c6cd0178d0831c62c7591217
Chájù <61478663+jarod-2134@users.noreply.github.com>
2022-04-11 15:34:17 +0200

    Update FindFlower.java

--- Design greenfoot/FindFlower.java
+++ Design greenfoot/FindFlower.java
@@ -2,4 +2,28 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class FindFlower implements FindInterface {
     private Actor actor;
+    
+    public void find (Actor actor);
+    
+    public void pickflowers()
+    {
+        if(canSee(Flower.class))
+        {
+            Flower flower = (Flower) getOneObjectAtOffset(0, 0, Flower.class);
+            if(flower.getPoisonous())
+            {
+                removeObject (LittleRedCap.class);
+                score=score-3;
+
+            }else {
+                score++;
+            }
+            removeObject (Flower.class);
+            Level1 Level1 = (Level1)getWorld();
+            World world = getWorld();
+            world.removeObjects(world.getObjects(Scoreboard.class));
+            world.addObject (new Scoreboard(score), 75,25);
+
+        }
+    }
 }
