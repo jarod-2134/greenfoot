012980655e700e111ca811274c034267dc67b908
Chájù <61478663+jarod-2134@users.noreply.github.com>
2022-04-11 15:43:27 +0200

    Update LittleRedCap.java

--- Design greenfoot/LittleRedCap.java
+++ Design greenfoot/LittleRedCap.java
@@ -8,12 +8,15 @@ import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class LittleRedCap extends Actor {
     private MoveInterface moveInterface;
-
+    private FindInterface findInterface;
+    
     public LittleRedCap() {
         moveInterface = new MoveWithArrowsAdapter();
+        findInterface = new FindFlower();
     }
 
     public void act() {
         moveInterface.move(this);
+        findInterface.find(this);
     }
 }
